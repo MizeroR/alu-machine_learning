0816465705f1f73eec6f3f9dfb0a09c1af6a909f
MizeroR <r.mizero@alustudent.com>
2025-05-24 15:06:01 +0200

    Fix: Documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,7 +2,12 @@
 """
 This module provides a function to determine the shape of a matrix.
 """
+
+
 def matrix_shape(matrix):
+    """
+    Returns the shape of a matrix as a list of dimensions.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
