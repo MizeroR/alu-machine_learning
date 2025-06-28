42dd3d20d844165b704a8f7db835f71e257709a8
MizeroR <r.mizero@alustudent.com>
2025-06-28 17:00:05 +0200

    (feat) correlation

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Calculates the correlation matrix from a covariance matrix.
+"""
+
+import numpy as np  # type: ignore
+
+def correlation(C):
+    """
+    Calculates a correlation matrix.
+
+    Args:
+        C (numpy.ndarray): shape (d, d), the covariance matrix
+
+    Returns:
+        numpy.ndarray: shape (d, d), the correlation matrix
+
+    Raises:
+        TypeError: if C is not a numpy.ndarray
+        ValueError: if C is not a 2D square matrix
+    """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    stddev = np.sqrt(np.diag(C))
+    denom = np.outer(stddev, stddev)
+
+    correlation_matrix = C / denom
+    np.fill_diagonal(correlation_matrix, 1.0)
+
+    return correlation_matrix
