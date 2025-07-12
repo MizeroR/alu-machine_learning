754ddb74c884440f3cd46dce85a7119b3c5e2525
MizeroR <r.mizero@alustudent.com>
2025-07-12 16:09:40 +0200

    fix: pycodestyle validation

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 
+
 def convolve_grayscale_padding(images, kernel, padding):
     """
     Performs a convolution on grayscale images with custom padding.
@@ -20,7 +21,7 @@ def convolve_grayscale_padding(images, kernel, padding):
     ph, pw = padding
 
     # Pad images with zeros
-    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    padded_images = np.pad(images,((0, 0),(ph, ph),(pw, pw)),mode='constant')
 
     # Compute output dimensions
     output_h = h + 2 * ph - kh + 1
