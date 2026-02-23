6330d823a9b5995d236accbc7a7f5f02c46be432
MizeroR <r.mizero@alustudent.com>
2026-02-23 21:58:47 +0200

    feat: add module for determining optimum number of clusters and main execution script

---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Module for determining the optimum number of clusters"""
+
+import numpy as np
+
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+    Tests for the optimum number of clusters by variance
+
+    Returns:
+    results, d_vars
+    or (None, None) on failure
+    """
+    if (not isinstance(X, np.ndarray) or len(X.shape) != 2):
+        return None, None
+
+    if (not isinstance(kmin, int) or kmin <= 0):
+        return None, None
+
+    if kmax is None:
+        kmax = X.shape[0]
+
+    if (not isinstance(kmax, int) or
+            kmax <= 0 or
+            kmax < kmin):
+        return None, None
+
+    if (kmax - kmin + 1) < 2:
+        return None, None
+
+    if (not isinstance(iterations, int) or iterations <= 0):
+        return None, None
+
+    results = []
+    variances = []
+
+    # LOOP 1 (allowed)
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+
+        if C is None:
+            return None, None
+
+        results.append((C, clss))
+        variances.append(variance(X, C))
+
+    # Compute deltas (vectorized, no loop)
+    variances = np.array(variances)
+    d_vars = variances[0] - variances
+
+    return results, d_vars.tolist()
